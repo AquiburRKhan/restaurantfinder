678722922b7c460ae541ef8d7d4a5abb4d955dc5
AquiburRKhan <ark.aquib17@gmail.com>
2018-11-21 13:46:05 +0600

    Updates

---
+++ precache-manifest.0711fc2f583401195491d1081079d151.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f3cf0025510d77a9159",
+    "url": "/restaurantfinder/static/js/runtime~main.7f3cf002.js"
+  },
+  {
+    "revision": "8b1604f987d1d053b254",
+    "url": "/restaurantfinder/static/js/main.8b1604f9.chunk.js"
+  },
+  {
+    "revision": "a3cde6abd3526f7faea0",
+    "url": "/restaurantfinder/static/js/1.a3cde6ab.chunk.js"
+  },
+  {
+    "revision": "8b1604f987d1d053b254",
+    "url": "/restaurantfinder/static/css/main.b8c8913d.chunk.css"
+  },
+  {
+    "revision": "b343151831f4e8523c02ed81ae6597fe",
+    "url": "/restaurantfinder/index.html"
+  }
+];
